84021a9807ec68259affa6d8a3dd9623a71369b5
Benedict Allerberger <me@bene.dev>
2022-07-30 21:38:42 +0200

    Add request validation

--- src/controllers/Colony.ts
+++ src/controllers/Colony.ts
@@ -1,3 +1,4 @@
+import { Rule, Validator } from "@cesium133/forgjs";
 import { Router, Request, Response, NextFunction } from "express";
 
 import { InvalidArgumentError } from "../errors";
@@ -30,10 +31,36 @@ router.get(
   }
 );
 
+const createColonyBodyRule = new Validator({
+  name: new Rule({ type: "string" }, "'name' missing"),
+  beekeeper: new Rule({ type: "string" }, "'beekeeper' missing"),
+  breed: new Rule({ type: "string" }, "'breed' missing"),
+  description: new Rule({ type: "string", optional: true }),
+  count: new Rule({ type: "int", min: 0 }, "'count' missing or invalid"),
+  flightRadius: new Rule(
+    { type: "int", min: 0 },
+    "'flightRadius' missing or invalid"
+  ),
+  latitude: new Rule(
+    { type: "string-float", min: 0 },
+    "'latitude' missing or invalid"
+  ),
+  longitude: new Rule(
+    { type: "string-float", min: 0 },
+    "'longitude' missing or invalid"
+  ),
+});
+
 router.post(
   "/",
   authorize(),
   async (req: Request, res: Response, next: NextFunction) => {
+    // Check body
+    const errors = createColonyBodyRule.getErrors(req.body);
+    if (errors.length !== 0) {
+      return void res.status(400).json({ error: errors.join(", ") });
+    }
+
     // Save field
     await prisma.colony.create({
       data: req.body,
